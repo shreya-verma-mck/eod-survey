0946affd2344a8c5136d35bb24946602350ba272
Shreya Verma <shreya_verma@mckinsey.com>
2023-02-20 17:37:04 +0530

    feat: Add form with input and radio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='content'>
+      <div>
+        <label for="keep">Keep comments for the day (What was good? What do we want to continue?)</label><br/>
+        <input name="keep" className="input-box" />
+        <div>
+          <p>How effective were today's sessions in helping your learning? Please rate on a scale of 1-5.</p>
+          <input type="radio" id="html" name="session_rating" value="1"/>
+          <label for="1">1 - Very ineffective</label><br/>
+          <input type="radio" id="css" name="session_rating" value="2"/>
+          <label for="2">2 - Ineffective</label><br/>
+          <input type="radio" id="javascript" name="session_rating" value="3"/>
+          <label for="3">3 - Somewhat ineffective</label>
+        </div>
+      </div>
     </div>
   );
 }
